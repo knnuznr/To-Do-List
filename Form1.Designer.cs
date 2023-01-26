a8efdfa353f6b931490858cc2d5c1164f62c96ef
knzrn <kaanuznr@gmail.com>
2023-01-26 21:14:47 +0300

    Changed text and location

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -54,11 +54,11 @@
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(216)))), ((int)(((byte)(185)))));
-			this.label1.Location = new System.Drawing.Point(26, 19);
+			this.label1.Location = new System.Drawing.Point(25, 19);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(84, 22);
+			this.label1.Size = new System.Drawing.Size(143, 22);
 			this.label1.TabIndex = 4;
-			this.label1.Text = "Planned";
+			this.label1.Text = "Planned Tasks";
 			// 
 			// pictureBox2
 			// 
